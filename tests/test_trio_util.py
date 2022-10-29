074dda968028c7c2df6726dca2f193f86123a3e3
Eric Lemoine <erik.lemoine@gmail.com>
2022-10-29 21:18:12 +0200

    Add trio_util.py unit tests.
    Fix an issue in the `ResultCapture.exception` property where 'done' test was performed on `_done_event.set()` rather than `_done_event.is-set()`.

---
+++ tests/test_trio_util.py
@@ -0,0 +1,76 @@
+import pytest
+import trio
+
+from trio_engineio.trio_util import (
+    ResultCapture,
+    TaskWrappedException,
+    TaskNotDoneException,
+)
+
+
+async def fn(arg):
+    await trio.sleep(1)
+    if arg == 0:
+        raise ValueError()
+    return 2 * arg
+
+
+class TestResultCapture:
+    async def test_start_soon(self, nursery):
+        t: ResultCapture = ResultCapture.start_soon(nursery, fn, 1)
+
+        assert t._routine == fn
+        assert t._args == (1,)
+        assert not t._has_run_been_called
+        assert not t._done_event.is_set()
+        assert t._result is None
+        assert t._exception is None
+
+    async def test_run_already_called(self, autojump_clock):
+        async with trio.open_nursery() as nursery:
+            t: ResultCapture = ResultCapture.start_soon(nursery, fn, 1)
+            await t.wait_done()
+            with pytest.raises(
+                RuntimeError, match=r"ResultCapture.run\(\) called multiple times"
+            ):
+                await t.run()
+
+    async def test_result(self, nursery, autojump_clock):
+        t: ResultCapture = ResultCapture.start_soon(nursery, fn, 1)
+        await t.wait_done()
+
+        assert t.result == 2
+        assert t.done
+
+    async def test_result_not_done(self, nursery, autojump_clock):
+        t: ResultCapture = ResultCapture.start_soon(nursery, fn, 1)
+
+        with pytest.raises(TaskNotDoneException):
+            _result = t.result
+
+    async def test_result_exception(self, autojump_clock):
+        try:
+            async with trio.open_nursery() as nursery:
+                t: ResultCapture = ResultCapture.start_soon(nursery, fn, 0)
+            await t.wait_done()
+        except BaseException:
+            pass
+        finally:
+            with pytest.raises(TaskWrappedException):
+                _result = t.result
+
+    async def test_exception(self, autojump_clock):
+        try:
+            async with trio.open_nursery() as nursery:
+                t: ResultCapture = ResultCapture.start_soon(nursery, fn, 0)
+            await t.wait_done()
+        except BaseException:
+            pass
+        finally:
+            assert isinstance(t.exception, ValueError)
+
+    async def test_exception_not_done(self, nursery, autojump_clock):
+        t: ResultCapture = ResultCapture.start_soon(nursery, fn, 1)
+
+        with pytest.raises(TaskNotDoneException):
+            _exc = t.exception
