b66fb69cd4c5cdf8a5a5685c76e9ce7976e000cb
Eric Lemoine <erik.lemoine@gmail.com>
2022-10-29 22:42:12 +0200

    Add License information.

--- src/trio_engineio/trio_util.py
+++ src/trio_engineio/trio_util.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2022, Eric Lemoine
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 """
 From: https://github.com/groove-x/trio-util/issues/20
 with code in https://gist.github.com/arthur-tacca/32c9b5fa81294850cabc890f4a898a4e
