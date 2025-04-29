978dab21b3f513afa2376d60da0148e520bfb619
Eric Lemoine <erik.lemoine@gmail.com>
2025-04-29 17:58:35 +0200

    Delete dependabot.yml Update volumio ip

--- Examples/test_trio_client.py
+++ Examples/test_trio_client.py
@@ -80,7 +80,7 @@ async def main():
         try:
             await eio.connect(
                 nursery,
-                "http://192.168.0.39:3000",
+                "http://192.168.1.106:3000",
                 # "http://192.168.0.39:3000/socket.io",
                 # transports="websocket",
                 # transports=["websocket"],
